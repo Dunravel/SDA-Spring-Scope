b07519fa4520065898fd1e2ca615901d5e1d8d6a
Marcin Iskra <dunravel@gmail.com>
2019-03-02 14:50:14 +0100

    Initial commit

---
+++ src/main/java/pl/sda/spring/scopedemo/Dependency.java
@@ -0,0 +1,4 @@
+package pl.sda.spring.scopedemo;
+
+public class Dependency {
+}
---
+++ src/test/java/pl/sda/spring/scopedemo/ScopeDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.spring.scopedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScopeDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
