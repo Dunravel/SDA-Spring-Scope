be29f4fa5b866faf9cb7870fc1fee212614f36fb
Marcin Iskra <dunravel@gmail.com>
2019-03-02 14:50:58 +0100

    Added context variable

--- src/main/java/pl/sda/spring/scopedemo/ScopeDemoApplication.java
+++ src/main/java/pl/sda/spring/scopedemo/ScopeDemoApplication.java
@@ -2,12 +2,14 @@ package pl.sda.spring.scopedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ScopeDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScopeDemoApplication.class, args);
+        ConfigurableApplicationContext con = SpringApplication.run(ScopeDemoApplication.class, args);
+
     }
 
 }
